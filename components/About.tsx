05b06d7faf08d8b95d146bed936171c387509770
Agam Jain <jainagam4@gmail.com>
2024-07-28 13:49:46 +0530

    feat: Add About component with profile image and technology list

---
+++ components/About.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { AiFillThunderbolt } from 'react-icons/ai';
+import Image from 'next/image';
+import SectionTitle from './SectionTitle'; 
+import { profileImg } from "@/public/assets";
+
+
+const About = () => {
+  return (
+    <>
+      <section
+      id="about"
+      className="max-w-containerSmall mx-auto py-10 lgl:py-32 flex flex-col gap-8"
+    >
+      <SectionTitle title="About Me" titleNo="01" />
+      <div className="flex flex-col lgl:flex-row gap-16">
+        <div className="w-full lgl:w-2/3 text-base text-textDark font-medium flex flex-col gap-4">
+          <p>
+            Hello! I'm Agam Jain. My passion for bringing ideas to life on the web began in early 2022 when I enrolled in college. Additionally, since the beginning of the 10th grade, I have been creating various projects and have developed a strong interest in researching new topics across different fields.
+          </p>
+          <p>
+            I pursued my passion for video editing and was fortunate to complete an internship{" "}
+            <span className="text-textGreen">
+              with a well-respected digital marketing agency. Now, I'm excited to fully focus on my journey in web development!
+            </span>
+          </p>
+          <p>Here are a few technologies I have been working with recently:</p>
+          <ul className="max-w-[450px] text-sm font-titleFont grid grid-cols-3 gap-2 mt-6">
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              JavaScript (ES6+)
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Next.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              React
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Node.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              TypeScript
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Express.js
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              MongoDB
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Tailwindcss
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              SvelteKit
+            </li>
+            <li className="flex items-center gap-2">
+              <span className="text-textGreen">
+                <AiFillThunderbolt />
+              </span>
+              Redis
+            </li>
+          </ul>
+        </div>
+        <div className="w-full lgl:w-1/3 h-80 relative group">
+          <div className="absolute w-full h-80 -left-6 -top-6 rounded-lg ">
+            <div className="w-full h-full relative z-20 flex pl-6 lgl:pl-0">
+              <Image
+                className="rounded-lg h-full object-cover"
+                src={profileImg}
+                alt="profileImg"
+              />
+              <div className="hidden lgl:inline-block absolute w-full h-80 bg-textGreen/20 rounded-md top-0 left-0 group-hover:bg-transparent duration-300"></div>
+            </div>
+          </div>
+          <div className="hidden lgl:inline-flex w-full h-80 border-2 border-textGreen rounded-md group-hover:-translate-x-2 group-hover:-translate-y-2 transition-transform duration-300"></div>
+        </div>
+      </div>
+    </section>
+    </>
+    
+  );
+};
+
+export default About;
